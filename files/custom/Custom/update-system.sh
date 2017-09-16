98fdc5f93fbb30d4db1c1dc4858c0b3d71a97454
Gaetan <gaetan_934@yahoo.fr>
2017-09-17 00:47:40 +0200

    Replaced apt-get with apt

--- files/custom/Custom/update-system.sh
+++ files/custom/Custom/update-system.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
 # Command that upgrades the software packages and the system. It also performs clean-up operation for aptitude
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get clean && sudo apt-get autoclean
+sudo apt update && sudo apt upgrade && sudo apt dist-upgrade && sudo apt clean && sudo apt autoclean
